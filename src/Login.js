537ae1d7613a64d0b41193112a7e95887a9bb0c0
e-unicode <yangzzanga@gmail.com>
2023-02-16 23:48:10 +0900

    로그인 버튼 수정

--- src/Login.js
+++ src/Login.js
@@ -5,15 +5,16 @@ function Login() {
   return (
     <div className="search">
       <div className="form">
-        <input className="input" placeholder="ID" required="" type="input"/>
+        <input className="input" placeholder="ID" required="" type="input" />
         <span className="input-border"></span>
       </div>
       <div className="form">
-        <input className="input" placeholder="PASSWORD" required="" type="input"/>
+        <input className="input" placeholder="PASSWORD" required="" type="input" />
         <span className="input-border"></span>
       </div>
       <div>
-        <button className="enter">Enter</button>
+        <button className="enter">Sign in</button>
+        <button className="enter" style={{ marginLeft: "2%" }}>Join</button>
       </div>
     </div>
   );
