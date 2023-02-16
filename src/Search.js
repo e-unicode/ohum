4648ba8e0331a9a353a75fa99484df1ea5bcc2f0
e-unicode <yangzzanga@gmail.com>
2023-02-17 02:32:11 +0900

    search-db 연결

--- src/Search.js
+++ src/Search.js
@@ -4,13 +4,15 @@ import { useEffect, useState } from "react";
 function Search() {
   return (
     <div className="search">
-      <div className="form">
-        <input className="input" placeholder="Search for" required="" type="input"/>
-        <span className="input-border"></span>
-      </div>
-      <div>
-        <button className="enter">Enter</button>
-      </div>
+      <form action="/" method="POST">
+        <div className="form">
+          <input className="input" placeholder="Search for" required="" type="text" name="search" />
+          <span className="input-border"></span>
+        </div>
+        <div>
+          <button className="enter" type="submit">Enter</button>
+        </div>
+      </form>
     </div>
   );
 }
