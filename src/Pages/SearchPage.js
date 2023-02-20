d273b8ec6ad8db2983f58803623425f7d5281766
e-unicode <yangzzanga@gmail.com>
2023-02-21 01:56:53 +0900

    서치페이지 구현

--- src/Pages/SearchPage.js
+++ src/Pages/SearchPage.js
@@ -1,7 +1,8 @@
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, InputGroup, FormControl, Button, Row, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
+import Search from "../Components/Search";
+import Banner from "../Components/Banner";
 
 function SearchPage(props) {
   const [searchInput, setSearchInput] = useState("");
@@ -9,54 +10,35 @@ function SearchPage(props) {
   return (
     <div className="post">
       <div className="post-top">
-        <div className="search-form">
-          <div className="form" style={{ width: "100%" }}>
-            <input
-              className="search-input"
-              type="input"
-              onKeyPress={(e) => {
-                if (e.key === "Enter") {
-                  console.log("pressed");
-                }
-              }}
-              onChange={(e) => setSearchInput(e.target.value)}
-              placeholder="노래 제목, 가수 이름, 관련 키워드를 입력하세요."
-              required=""
-            />
-            <button
-              onClick={() => {
-                console.log("clicked");
-              }}
-              className="search-enter"
-            >
-              찾기
-            </button>
-          </div>
-        </div>
+        <Search />
       </div>
 
-      <div className="post-content pt-5">
-        <div className="content-side">
-          <div className="side-btn">메인피드페이지</div>
-          <div className="side-btn">나랑 같은 날씨</div>
-          <div className="side-btn">나랑 같은 위치</div>
-          <div className="side-btn">내 페이지</div>
+      <div className="post-content">
+        <div className="post-content-side">
+          <Banner />
         </div>
-        <div className="content-main">
-          <Container>
-            <Row className="mx-2 row row-cols-4">
+
+        <div className="post-content-main">
+          <div className="search-result">
+            <h2 style={{ color: "#F2F2F2", marginBottom: "10px", fontWeight: "900" }}>Albums</h2>
+            <div className="post-content-result">
               {props.albums.map((album, i) => {
                 return (
-                  <Card>
-                    <Card.Img src={album.images[0].url} />
-                    <Card.Body>
-                      <Card.Title>{album.name}</Card.Title>
-                    </Card.Body>
-                  </Card>
+                  <div className="result-box">
+                    <div className="result-box-card">
+                      <div className="result-box-card-cover">
+                        <img src={album.images[0].url} />
+                      </div>
+                      <div className="result-box-card-title">
+                        <p style={{ fontWeight: "700" }}>{album.name}</p>
+                        <p>{album.name}</p>
+                      </div>
+                    </div>
+                  </div>
                 );
               })}
-            </Row>
-          </Container>
+            </div>
+          </div>
         </div>
       </div>
     </div>
