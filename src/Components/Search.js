bf8c19066038f831e916a5b0f73fcd9327b8a814
e-unicode <yangzzanga@gmail.com>
2023-02-22 01:50:39 +0900

    modify search page result album cover

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,6 @@
 import "../App.css";
-import { useState } from "react";
 
-function Search() {
-  const [searchInput, setSearchInput] = useState("");
+function Search(props) {
   return (
     <div className="search-form">
       <div className="form" style={{ width: "100%" }}>
@@ -11,17 +9,15 @@ function Search() {
           type="input"
           onKeyPress={(e) => {
             if (e.key === "Enter") {
-              console.log("pressed");
+              props.searchSpotify();
             }
           }}
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => props.setSearchInput(e.target.value)}
           placeholder="노래 제목, 가수 이름, 관련 키워드를 입력하세요."
           required=""
         />
         <button
-          onClick={() => {
-            console.log("clicked");
-          }}
+          onClick={props.searchSpotify}
           className="search-enter"
         >
           찾기
